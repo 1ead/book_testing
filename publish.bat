0a247a65cc31335125d35dfdfb652445c5c69b27
_meugen_ <eugen35@mail.ru>
2016-10-04 05:44:36 +0500

    Пробую сделать батник

--- publish.bat
+++ publish.bat
@@ -1,7 +1,10 @@
-  git checkout master
-  git checkout gh-pages
-  xcopy _book\*.* . /e /y
-  git add --all
-  git commit -m "pushing new content"
-  git pull origin gh-pages
-  git push origin gh-pages
+   git checkout master
+   git checkout gh-pages
+   xcopy _book\*.* . /e /y
+   git add --all
+   git commit -m "pushing new content"
+   git pull origin gh-pages
+   git add --all
+   git commit -m "pushing new content"
+   git push origin gh-pages
+   git checkout master
